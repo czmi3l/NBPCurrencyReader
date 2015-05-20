9dc5ad77de98d036e2b0dd6b85a3efbf803d8c54
czmi3l <czmiel1224@gmail.com>
2015-05-20 11:20:06 +0200

    v 1.0.0

--- NBPCurrencyReader/Program.cs
+++ NBPCurrencyReader/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NBPCurrencyReader
 {
@@ -10,6 +6,29 @@ namespace NBPCurrencyReader
     {
         static void Main(string[] args)
         {
+            string start;
+            string end;
+            string currency;
+
+            //Take data from user
+            Console.WriteLine("NBP Currency Reader v1.0");
+            Console.WriteLine("Enter currency(USD, EUR, CHF, GBP):");
+            currency = Console.ReadLine();
+            Console.WriteLine("Enter start date(dd-mm-rr):");
+            start = Console.ReadLine();
+            Console.WriteLine("Enter end date(dd-mm-rr):");
+            end = Console.ReadLine();
+
+            //Create major object
+            var value = new Value(currency, start, end);
+
+            value.DownloadAllNameFiles();
+            value.ParseRequiredNameFiles();
+            value.DownloadPurchaseAndSellingPrice();
+
+            Console.WriteLine("Average: {0:N4}", value.CalculateAverageRateOfPurchasePrices());
+            Console.WriteLine("Standard deviation: {0:N4}", value.CalculateStandardDeviationOfSellingPrices());
+            Console.Read();
         }
     }
 }
